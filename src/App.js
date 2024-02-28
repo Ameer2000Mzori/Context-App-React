6c9fc0db18ebc94624a43575c06d4568c80a9167
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-28 06:28:06 +0100

    added global context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import react from 'react'
+import { createContext } from 'react'
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 import Home from './components/pages/Home.jsx'
 import About from './components/pages/About.jsx'
 import Contact from './components/pages/Contact.jsx'
 import NotFound from './components/pages/NotFound.jsx'
+import { getByDisplayValue } from '@testing-library/react'
+
+export const Informations = createContext()
+
+function Card() {
+  console.log('Card is being rendered') // Debugging statement
+  return (
+    <div style={{ border: '2px solid red', padding: '20px', margin: '20px' }}>
+      <h1>this is text of using components inside of context</h1>
+    </div>
+  )
+}
+
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
+      <Informations.Provider
+        value={{ name: 'Ameer', age: 19, gender: 'male', Card }}
+      >
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </Informations.Provider>
     </>
   )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Informations } from '../../App.js'
 
 const Home = () => {
+  const { name, age, gender, Card } = useContext(Informations)
+  console.log(name, age, gender, Card)
+
   return (
-    <div className="flex flex-col text-center items-center justify-center h-[100vh] w-[100vw]">
-      Home
-    </div>
+    <>
+      <div className="flex flex-col text-center items-center justify-center h-[100vh] w-[100vw]">
+        Home
+      </div>
+      <Card />
+    </>
   )
 }
 
